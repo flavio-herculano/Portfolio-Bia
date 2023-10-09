a71437a14789928c1ed7cf2d1913acc7eb742c91
Flávio Herculano <flavio.hercullano@gmail.com>
2023-10-09 04:34:53 -0300

    refactor code to have translations on not-found

--- app/[locale]/not-found.tsx
+++ app/[locale]/not-found.tsx
@@ -1,21 +1,20 @@
 import Link from "next/link";
+import { useTranslations } from "next-intl";
 
 export default function NotFound() {
+  const NotFound = useTranslations("NotFound");
   return (
     <div className="min-h-screen flex justify-center items-center">
       <div className="max-w-screen-sm p-4">
-        <h1 className="text-5xl">Oopa! Erro 404</h1>
-        <h2 className="mt-8 text-2xl">Página não encontrada,</h2>
-        <p className="my-2 text-lg font-light">
-          Parece que você se aventurou em território desconhecido. A página que
-          você estava procurando não foi encontrada.
-        </p>
+        <h1 className="text-5xl">{NotFound("error")}</h1>
+        <h2 className="mt-8 text-2xl">{NotFound("page")}</h2>
+        <p className="my-2 text-lg font-light">{NotFound("looks")}</p>
         <div className="my-8">
           <Link
             className="button-primary text-center text-white font-bold py-4 px-16 rounded-full"
             href="/"
           >
-            Voltar
+            {NotFound("return")}
           </Link>
         </div>
       </div>
