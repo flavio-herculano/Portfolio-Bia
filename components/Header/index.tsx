a40d4d27b7d658a8692a8a8c590e48c15fb484ea
Flávio Herculano <81449619+flavio-herculano@users.noreply.github.com>
2023-10-12 04:41:19 -0300

    added next/font/google

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,14 +1,21 @@
 import { useTranslations } from "next-intl";
 import Link from "next/link";
+import { Itim } from "next/font/google";
+import { Inter } from "next/font/google";
+
+export const itim = Itim({ weight: "400", subsets: ["latin"] });
+export const inter = Inter({ subsets: ["latin"] });
 
 function Header() {
   const H = useTranslations("Header");
   return (
     <header className="container mx-auto flex flex-row justify-between py-5">
-      <div className="max-md:px-2 logo text-2xl text-[#1F3041]">
+      <div className={`max-md:px-2 ${itim.className} text-2xl text-[#1F3041]`}>
         <Link href="/">Bianca Herculano UX</Link>
       </div>
-      <ul className="max-md:hidden md:gap-x-5 flex text-[#65687D] flex-rol gap-x-10 font-['Inter'] font-medium items-center">
+      <ul
+        className={`max-md:hidden md:gap-x-5 flex text-[#65687D] flex-rol gap-x-10 ${inter.className} font-medium items-center`}
+      >
         <li className="cursor-pointer">
           <Link href="/pt" locale="pt">
             {H("pt")}
@@ -21,7 +28,7 @@ function Header() {
         </li>
         <li className="cursor-pointer">{H("about")}</li>
         <li className="cursor-pointer">{H("contact")}</li>
-        <li className="text-white bg-[--primary-color] p-2 rounded-3xl px-8 py-2 hover:opacity-75 transition-opacity ease-in-out delay-150 duration-300 cursor-pointer">
+        <li className="text-white bg-[--primary-color] p-2 rounded-3xl px-8 py-2 hover:opacity-70 transition-opacity ease-in-out delay-150 duration-300 cursor-pointer">
           {H("contact me")}
         </li>
       </ul>
