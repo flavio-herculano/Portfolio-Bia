9ca3b7a70c827e1baae7dc32e45cd5c29c165945
Flávio Herculano <81449619+flavio-herculano@users.noreply.github.com>
2023-10-12 03:39:55 -0300

    added Header with translates and responsive

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,17 +1,30 @@
-import styles from "./styles.module.css";
+import { useTranslations } from "next-intl";
+import Link from "next/link";
 
 function Header() {
+  const H = useTranslations("Header");
   return (
-    <header className=" md:grid-cols-12 max-md:grid-cols-6 container mx-auto grid gap-4">
-      <div className="md:col-span-3 max-md:col-span-2 text-left py-4">
-        Bianca Herculano UX
+    <header className="container mx-auto flex flex-row justify-between py-5">
+      <div className="max-md:px-2 logo text-2xl text-[#1F3041]">
+        <Link href="/">Bianca Herculano UX</Link>
       </div>
-      <div className="md:col-span-9 max-md:col-span-4 text-right py-4 flex flex-rol justify-end gap-x-10">
-        <p>Home</p>
-        <p>Sobre mim</p>
-        <p>Contato</p>
-        <button>Contact me</button>
-      </div>
+      <ul className="max-md:hidden flex text-[#65687D] flex-rol gap-x-10 font-['Inter'] font-medium items-center">
+        <li>
+          <Link href="/pt" locale="pt">
+            {H("pt")}
+          </Link>
+        </li>
+        <li>
+          <Link href="/en" locale="en">
+            {H("en")}
+          </Link>
+        </li>
+        <li>{H("about")}</li>
+        <li>{H("contact")}</li>
+        <li className="text-white bg-[--primary-color] p-2 rounded-3xl px-8 py-2">
+          {H("contact me")}
+        </li>
+      </ul>
     </header>
   );
 }
