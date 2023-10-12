150971e32fb57a4faac7ec0a41a73b8df5e20037
Flávio Herculano <81449619+flavio-herculano@users.noreply.github.com>
2023-10-12 03:57:21 -0300

    fixed css md:gap

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -8,20 +8,20 @@ function Header() {
       <div className="max-md:px-2 logo text-2xl text-[#1F3041]">
         <Link href="/">Bianca Herculano UX</Link>
       </div>
-      <ul className="max-md:hidden flex text-[#65687D] flex-rol gap-x-10 font-['Inter'] font-medium items-center">
-        <li>
+      <ul className="max-md:hidden md:gap-x-5 flex text-[#65687D] flex-rol gap-x-10 font-['Inter'] font-medium items-center">
+        <li className="cursor-pointer">
           <Link href="/pt" locale="pt">
             {H("pt")}
           </Link>
         </li>
-        <li>
+        <li className="cursor-pointer">
           <Link href="/en" locale="en">
             {H("en")}
           </Link>
         </li>
-        <li>{H("about")}</li>
-        <li>{H("contact")}</li>
-        <li className="text-white bg-[--primary-color] p-2 rounded-3xl px-8 py-2">
+        <li className="cursor-pointer">{H("about")}</li>
+        <li className="cursor-pointer">{H("contact")}</li>
+        <li className="text-white bg-[--primary-color] p-2 rounded-3xl px-8 py-2 hover:opacity-75 transition-opacity ease-in-out delay-150 duration-300 cursor-pointer">
           {H("contact me")}
         </li>
       </ul>
